3ad985950192277e40fc8e6083c7822b958f70f4
trendex2030 <bravinnyakundi674@gmail.com>
2025-06-28 16:20:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -38,6 +38,6 @@ CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
-TREND-X: process.env.TREND-X || "『TREND-X』",
+'TREND-X': process.env.TREND_X || "『TREND-X』",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
