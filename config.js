24827db028acbacbd9962f7c4721bfab4c56432e
trendex2030 <bravinnyakundi674@gmail.com>
2025-07-05 20:57:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,3 @@
-/**
-
-
- Copyright (C) 2025.
- Licensed under the  GPL-3.0 License;
- You may not sell this script.
- It is supplied in the hope that it may be useful.
- * @project_name : Free Bot script
- * @author : Malvin King <https://github.com/kingmalvn>
- * @description : A Multi-functional whatsapp bot script.
- * @version 3.0.0
- **/
-
-
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
