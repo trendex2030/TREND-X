62b815556bb773ff1150f6081f2324cb80196430
trendex2030 <bravinnyakundi674@gmail.com>
2025-07-20 15:10:56 +0300

    Update trend.js

--- trend.js
+++ trend.js
@@ -1,20 +1,20 @@
 var commands = [];
 
 function ven(info, func) {
-    var data = info;
-    data.function = func;
-    if (!data.dontAddCommandList) data.dontAddCommandList = false;
-    if (!info.desc) info.desc = '';
-    if (!data.fromMe) data.fromMe = false;
-    if (!info.category) data.category = 'misc';
-    if (!info.filename) data.filename = "Not Provided";
-    commands.push(data);
-    return data;
+var data = info;
+data.function = func;
+if (!data.dontAddCommandList) data.dontAddCommandList = false;
+if (!info.desc) info.desc = '';
+if (!data.fromMe) data.fromMe = false;
+if (!info.category) data.category = 'misc';
+if(!info.filename) data.filename = "Not Provided";
+commands.push(data);
+return data;
 }
-
 module.exports = {
-    AddCommand: ven,
-    Function: ven,
-    Module: ven,
-    commands,
+ven,
+AddCommand:ven,
+Function:ven,
+Module:ven,
+commands,
 };
