88e975ef42986373623c5ddfd21ba3a441e82f0f
trendex2030 <bravinnyakundi674@gmail.com>
2025-07-25 13:43:25 +0300

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, q, reply }) => {
 > 💨 *Wind Speed*: ${data.wind.speed} m/s
 > 🔽 *Pressure*: ${data.main.pressure} hPa
 
-> *© Powdered By ʜᴜɴᴛᴇʀ xᴍᴅ
+> *© Powdered By TREND-X
 `;
         return reply(weather);
     } catch (e) {
