1c85950dd3f7cdece54235f7e0a151011c191fe0
trendex2030 <bravinnyakundi674@gmail.com>
2025-07-25 13:31:36 +0300

    Update Quran.js

--- plugins/Quran.js
+++ plugins/Quran.js
@@ -449,15 +449,15 @@ async (conn, mek, m, { from, quoted, body,isCmd, command, args, q, isGroup, send
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://res.cloudinary.com/dgy2dutjs/image/upload/v1751707342/url.crissvevo.co.tz/%E1%B4%8F%CA%99%E1%B4%87%E1%B4%85%E1%B4%9B%E1%B4%87%E1%B4%84%CA%9C1_exmbht.jpg` },
+                image: { url: `https://files.catbox.moe/adymbp.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363416335506023@newsletter',
-                        newsletterName: '120363416335506023',
+                        newsletterJid: '120363401765045963@newsletter',
+                        newsletterName: 'TREND-X',
                         serverMessageId: 143
                     }
                 }
