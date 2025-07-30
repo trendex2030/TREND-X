5f53d608db0b45d991d6f017aa3afea77834ad29
trendex2030 <bravinnyakundi674@gmail.com>
2025-07-30 17:46:25 +0300

    Update emix-fun.js

--- plugins/emix-fun.js
+++ plugins/emix-fun.js
@@ -31,7 +31,7 @@ cmd({
         let buffer = await getBuffer(imageUrl);
         let sticker = new Sticker(buffer, {
             pack: "Emoji Mix",
-            author: "CRISS-AI",
+            author: "TREND-X",
             type: StickerTypes.FULL,
             categories: ["🤩", "🎉"],
             quality: 75,
