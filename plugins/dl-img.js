2b1398627477412d19ba66d31387840814eea266
trendex2030 <bravinnyakundi674@gmail.com>
2025-07-30 17:12:04 +0300

    Update dl-img.js

--- plugins/dl-img.js
+++ plugins/dl-img.js
@@ -37,7 +37,7 @@ cmd({
                 from,
                 { 
                     image: { url: imageUrl },
-                    caption: `📷 Result for: ${query}\n> © Powered by ʜᴜɴᴛᴇʀ xᴍᴅ`
+                    caption: `📷 Result for: ${query}\n> © Powered by TREND X`
                 },
                 { quoted: mek }
             );
