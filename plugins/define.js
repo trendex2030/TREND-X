578d4df1ac0d74b6eb4334d40d3b1c90b30883ec
trendex2030 <bravinnyakundi674@gmail.com>
2025-07-30 17:13:20 +0300

    Update define.js

--- plugins/define.js
+++ plugins/define.js
@@ -31,7 +31,7 @@ async (conn, mek, m, { from, q, reply }) => {
 ✍️ *Example*: ${example}  
 📝 *Synonyms*: ${synonyms}  
 
-🔗 *Powered By Criss Vevo*`;
+🔗 *Powered By TREND-X*`;
 
         if (audio) {
             await conn.sendMessage(from, { audio: { url: audio }, mimetype: 'audio/mpeg' }, { quoted: mek });
