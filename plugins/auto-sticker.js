34af29b9be63e57e5be7b807b017648535f54492
trendex2030 <bravinnyakundi674@gmail.com>
2025-07-30 17:34:55 +0300

    Update auto-sticker.js

--- plugins/auto-sticker.js
+++ plugins/auto-sticker.js
@@ -6,7 +6,7 @@ cmd({
   on: 'body'
 }, async (conn, mek, m, { from, body }) => {
   try {
-    const jsonUrl = 'https://raw.githubusercontent.com/criss-vevo/CRISS-DATA/main/autosticker.json';
+    const jsonUrl = 'https://raw.githubusercontent.com/trendex2030/TREND-X/main/data/autosticker.json';
     const res = await axios.get(jsonUrl);
     const data = res.data;
 
@@ -17,7 +17,7 @@ cmd({
             from,
             {
               sticker: { url: data[keyword] },
-              package: 'CRISS-AI'
+              package: 'TREND-X'
             },
             { quoted: mek }
           );
