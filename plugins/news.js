9aef99f2c6add2cd935fb955ff9c908f87bafba6
trendex2030 <bravinnyakundi674@gmail.com>
2025-08-03 20:55:04 +0300

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʜᴜɴᴛᴇʀ xᴍᴅ
+  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ TREND-X
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
