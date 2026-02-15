b97a827f627c0c9b765e0fd0b859e8c8f94817e4
trendex2030 <bravinnyakundi674@gmail.com>
2026-02-15 15:29:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ ANTI_LINK: process.env.ANTI_LINK || "false",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-    MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/j2h8dg.jpg",
+    MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://imgur.com/a/tPLIVnb",
 
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
